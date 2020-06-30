6ddf0c28af9b88d9fde037cb505015681048a79f
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2020-06-30 16:27:16 +1000

    Converted app and deps into func comp with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import axios from 'axios';
-import SearchBar from './SearchBar';
-import Number from './Number';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+import Number from "./Number";
 
-class App extends React.Component {
-  state = { number: '' };
+const App = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [numberQuery, setNumberQuery] = useState("");
+  const [numberTrivia, setNumberTrivia] = useState("");
+  const [performQuery, setPerformQuery] = useState(false);
 
-  onSearchSubmit = async (numberSel, option) => {
-    const response = await axios.get(
-      `http://numbersapi.com/${numberSel}/${option}`
-    );
+  // eslint-disable-next-line 
+  useEffect(() => {
+    if (performQuery) {
+      async function onSearchSubmit() {
+        const url = "http://numbersapi.com/";
+        const response = await axios.get(
+          `${url}${numberQuery}/${selectedOption}`
+        );
+        setNumberTrivia(response.data);
+        console.log("Performing query...");
+        console.log(numberQuery);
+        console.log(selectedOption);
+      }
+      onSearchSubmit();
+      setPerformQuery(false);
+    }
+  }, [performQuery]);
 
-    this.setState({ number: response.data });
-  };
-
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <Number number={this.state.number} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui container" style={{ marginTop: "10px" }}>
+      <SearchBar
+        query={setNumberQuery}
+        queryFlag={setPerformQuery}
+        option={setSelectedOption}
+      />
+      <Number number={numberTrivia} />
+    </div>
+  );
+};
 
 export default App;
+
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,65 +1,75 @@
-import React from 'react';
-import { Dropdown } from 'semantic-ui-react';
+import React, { useState}  from 'react';
 
-class SearchBar extends React.Component {
-  state = { numberSel: '', option: '' };
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
 
+const SearchBar = (props) => {
+  const setNumberQuery = props.query;
+  const setPerformQuery = props.queryFlag;
+  const setSelectedOption = props.option
+  const [numberInput, setNumberInput] = useState('');
+
+  const numberOptions = [
+    {
+      key: "trivia",
+      text: "Trivia",
+      value: "trivia",
+    },
+    {
+      key: "year",
+      text: "Year",
+      value: "year",
+    },
+    {
+      key: "date",
+      text: "Date",
+      value: "date",
+    },
+    {
+      key: "math",
+      text: "Math",
+      value: "math",
+    },
+  ];
+
+  
+  const readKeyInput = (event) => {
+    // event.preventDefault();
     if (event.keyCode === 13) {
-      this.props.onSubmit(this.state.numberSel, this.state.option);
+      setPerformQuery(true);
+      setNumberQuery(numberInput);
     }
   };
+  
+  return (
+    <div className="ui segment">
+      <div className="ui form" /*onSubmit={this.onFormSubmit}*/>
+        <div className="field">
+          <label>Search a number</label>
+          <select
+            placeholder="Select"
+            onChange={event => setSelectedOption(event.target.value)}
+            defaultValue={numberOptions[0].value}
+          >
+            {numberOptions.map(option =>
+            <option 
+            key={option.key} 
+            value={option.value}
+            >
+              {option.text}
+            </option>
 
-  render() {
-    const numberOptions = [
-      {
-        key: 'trivia',
-        text: 'Trivia',
-        value: 'trivia',
-      },
-      {
-        key: 'year',
-        text: 'Year',
-        value: 'year',
-      },
-      {
-        key: 'date',
-        text: 'Date',
-        value: 'date',
-      },
-      {
-        key: 'math',
-        text: 'Math',
-        value: 'math',
-      },
-    ];
-    return (
-      <div className="ui segment">
-        <div className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Search a number</label>
-            <Dropdown
-              placeholder="Select"
-              fluid
-              selection
-              options={numberOptions}
-              onChange={(e) =>
-                this.setState({ option: e.target.innerText.toLowerCase() })
-              }
-            />
-            <input
-              type="text"
-              value={this.state.numberSel}
-              onChange={(e) => this.setState({ numberSel: e.target.value })}
-              onKeyUp={this.onFormSubmit}
-            />
-          </div>
+            )}
+          </select>
+          <input
+            type="text"
+            value={numberInput}
+            onChange={(e) => setNumberInput(e.target.value)}
+            onKeyUp={readKeyInput}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SearchBar;
